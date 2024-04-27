1a14095fd66839caa9593eef9d7e49467340d564
PrithwirajBhadra <me.prithwiraj@gmail.com>
2024-04-27 07:28:54 +0530

    Header Sidebar loaded

--- src/Components/Router/AdminRouter.js
+++ src/Components/Router/AdminRouter.js
@@ -2,17 +2,21 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../Pages/Admin/Dashboard/Dashboard';
-import Login from '../Pages/Admin/UserManagement/Login';
-import Register from '../Pages/Admin/UserManagement/Register';
 import Category from '../Pages/Admin/Category/Category';
 import Question from '../Pages/Admin/Question/Question';
+import Header from '../Pages/Admin/Dashboard/Includes/Header';
+import Sidebar from '../Pages/Admin/Dashboard/Includes/Sidebar';
 const AdminRouter = () => {
   return (
+    <>
+    <Header />
+    <Sidebar />
     <Routes>
         <Route exact path="/" element={<Dashboard />} />
         <Route exact path="/category" element={<Category />} />
         <Route exact path="/question" element={<Question />} />
     </Routes>
+    </>
   );
 };
 
--- src/Components/Router/ClientRouter.js
+++ src/Components/Router/ClientRouter.js
@@ -2,8 +2,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Pages/User/Home/Home';
-import Login from '../Pages/User/User/Login';
-import Register from '../Pages/User/User/Register';
 
 const ClientRouter = () => {
   return (
