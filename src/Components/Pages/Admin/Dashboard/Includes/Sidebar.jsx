df1bcdca607db7ea48e58c1af50ee2bde45df6dd
PrithwirajBhadra <me.prithwiraj@gmail.com>
2024-05-26 11:32:13 +0530

    Merge conflict changes

--- src/Components/Pages/Admin/Dashboard/Includes/Sidebar.jsx
+++ src/Components/Pages/Admin/Dashboard/Includes/Sidebar.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link, redirect } from 'react-router-dom';
 import { useNavigate } from "react-router-dom"
-import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
    const navigate = useNavigate()
--- src/Components/services/api/index.js
+++ src/Components/services/api/index.js
@@ -7,7 +7,6 @@ export const APP_LAST_URI = Object.freeze({
         },
         signup : {
             path : 'v1/user/register-adminuser',
-            method : 'POST'
             method : 'POST',
             isAuth : true,
         },
@@ -66,49 +65,5 @@ export const APP_LAST_URI = Object.freeze({
             method : 'POST',
             isAuth : true,
         },
-        category_list : {
-            path : 'v1/category/category-list',
-            method : 'POST'
-        },
-        category_add : {
-            path : 'v1/category/category-add',
-            method : 'POST'
-        },
-        getCategoryById : {
-            path : 'v1/category/category-edit',
-            method : 'POST'
-        },
-        category_update : {
-            path : 'v1/category/category-update',
-            method : 'POST'
-        },
-        category_delete : {
-            path : 'v1/category/category-delete',
-            method : 'POST'
-        },
-        category_add_csv : {
-            path : 'v1/category/category-add-csv',
-            method : 'POST'
-        },
-        question_list : {
-            path : 'v1/question/question-list',
-            method : 'POST'
-        },
-        question_add : {
-            path : 'v1/question/question-add',
-            method : 'POST'
-        },
-        question_edit : {
-            path : 'v1/question/question-edit',
-            method : 'POST'
-        },
-        question_update : {
-            path : 'v1/question/question-update',
-            method : 'POST'
-        },
-        question_delete : {
-            path : 'v1/question/question-delete',
-            method : 'POST'
-        }
     }
 })
